c393776703d5fc5401960df0a03b1441dd803c77
Crystal Huang <chuan147@ucsc.edu>
2024-04-06 15:17:52 -0700

    initial commit

---
+++ asg0.js
@@ -0,0 +1,39 @@
+
+ // DrawRectangle.js
+ // Function to draw a vector on the canvas
+
+ function main() {
+    // Retrieve <canvas> element <- (1)
+    var canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+    canvas.style.backgroundColor = "black";
+    // Get the rendering context for 2DCG <- (2)
+    var ctx = canvas.getContext('2d');
+    var v1 = new Vector3([2.25,2.25,0])
+    // Draw a blue rectangle <- (3)
+    //ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color
+   // ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+
+    drawVector(v1, "red");
+}
+
+function drawVector(v,color) {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2,canvas.height / 2);
+    //ctx.translate(canvas.width / 2, canvas.height / 2);
+    var scaledX = v.elements[0] * 20;
+    var scaledY = v.elements[1] * 20
+    ctx.lineTo(canvas.width / 2 + scaledX,canvas.height / 2 - scaledY); // used ChatGPT to fix the proper repositioning
+    ctx.stroke();
+}
+
+function handleDrawEvent() {
+    let v1 = document.getElementById("name").value;
+    console.log(v1);
+}
