add41ffb4e9a9a807bbf724b9dc8792123789c22
Crystal Huang <chuan147@ucsc.edu>
2024-04-06 19:06:53 -0700

    completed part 5 i think

--- asg0.js
+++ asg0.js
@@ -54,5 +54,51 @@ function handleDrawEvent() {
     //console.log(yVal2);
     //console.log("clearing canvas!");
    
-    
+}
+function handleDrawOperationEvent() {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    // Specify the color for clearing <canvas>
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let xVal = document.getElementById("v1-xVal").value;
+    let yVal = document.getElementById("v1-yVal").value;
+    var v1 = new Vector3([xVal,yVal,0])
+    drawVector(v1, "red");
+
+    let xVal2 = document.getElementById("v2-xVal").value;
+    let yVal2 = document.getElementById("v2-yVal").value;
+    var v2 = new Vector3([xVal2,yVal2,0])
+    drawVector(v2, "blue");
+
+    //used ChatGPT to help with this portion
+    var opSelector = document.getElementById("op-select").value;
+    var v3;
+    var v4;
+    if (opSelector == "add") {
+        v3 = v1.add(v2);
+    } else if (opSelector == "sub") {
+        v3 = v1.sub(v2);
+    } else if (opSelector == "mul") {
+        var scalar = parseFloat(document.getElementById("scalar-val").value); // have to use parseFloat to do operations properly
+        v3 = v1.mul(scalar);
+        v4 = v2.mul(scalar);
+        //console.log(v3);
+        //console.log(v4);
+        
+    } else if (opSelector == "div") {
+        var scalar = parseFloat(document.getElementById("scalar-val").value); // have to use parseFloat to do operations properly
+        v3 = v1.div(scalar);
+        v4 = v2.div(scalar);
+        //console.log(v3);
+        //console.log(v4);
+    }
+    if (opSelector == "mul" || opSelector == "div" ) {
+        drawVector(v3, "green");
+        drawVector(v4, "green");
+    }else {
+        drawVector(v3, "green");
+    }
+
 }
